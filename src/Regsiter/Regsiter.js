3d28c292c5e20c0c81254c7b7deb99699c5ab93e
LocNguyen <boykute97hd@gmail.com>
2019-01-09 16:35:57 +0700

    update

---
+++ src/Regsiter/Regsiter.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+
+export default class Regsiter extends Component {
+    render() {
+      return (
+        <Text >eeeeeee</Text>
+      );
+    }
+  }

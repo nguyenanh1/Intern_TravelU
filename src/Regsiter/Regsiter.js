9952a7122774b2b181a967fe9ee14cc6e5426245
LocNguyen <boykute97hd@gmail.com>
2019-01-09 22:48:57 +0700

    View Resignter

--- src/Regsiter/Regsiter.js
+++ src/Regsiter/Regsiter.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
-import { Text,View,ImageBackground} from 'react-native';
+import { Text, View, ImageBackground, Image, TextInput } from 'react-native';
 import styles from './Style';
 
 export default class Regsiter extends Component {
-    render() {
-      return (
-        <View style={styles.khung}>
-            <View style={styles.top}>
-            <ImageBackground source={'../../resource'} style={{width: '100%', height: '100%'}}></ImageBackground>
-            </View>
+  render() {
+    return (
+      <View style={styles.khung}>
+        <View style={styles.top}>
+          <ImageBackground source={require('../../resource/image/backgrountoplogin.png')} style={{ width: '100%', height: '100%', }}>
+            <Image source={require('../../resource/image/ic_back.png')} ></Image>
+            <Image source={require('../../resource/image/logo.png')} style={{ marginLeft: '25%' }}></Image>
+          </ImageBackground>
         </View>
-      );
-    }
+
+        <View style={styles.body}>
+          <View style={styles.form}>
+
+            <View style={{ margin: '10%' }}>
+              <Text>Username</Text>
+              <TextInput
+                style={{ height: 40, width: '80%', borderColor: 'gray', borderBottomWidth: 0.5, fontSize: 20 }}
+                placeholder="example@gmail.com"
+              /></View>
+
+            <View style={{ margin: '10%' }}>
+              <Text>Password</Text>
+              <TextInput
+                style={{ height: 40, width: '80%', borderColor: 'gray', borderBottomWidth: 0.5, fontSize: 20 }}
+                placeholder="**********"
+              /></View>
+
+            <View style={{ margin: '10%' }}>
+              <Text>Repassword</Text>
+              <TextInput
+                style={{ height: 40, width: '80%', borderColor: 'gray', borderBottomWidth: 0.5, fontSize: 20 }}
+                placeholder="**********"
+              /></View>
+          </View>
+          <View style={styles.bot}>
+
+            <Image source={require('../../resource/image/button.png')} style={styles.anh}>
+            </Image>
+          </View>
+        </View>
+      </View >
+
+    );
   }
+}
--- src/Regsiter/Style.js
+++ src/Regsiter/Style.js
@@ -1,19 +1,35 @@
-import { StyleSheet,} from 'react-native';
+import { StyleSheet,backgroundColor} from 'react-native';
 const styles = StyleSheet.create({
+     
       khung: {
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
+         flex: 1,
+          
        },
        top: {
-           fontSize: 20,
-           textAlign: 'center',
-           margin: 10,
+           flex: 1 ,
+           position : "relative"
+
+           
       },
       body: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-   }
+
+        flex: 3 ,
+        backgroundColor: 'white',
+        
+      },
+      form: {
+         flex:6 , backgroundColor:'white',margin:15 , borderRadius: 30, paddingBottom:'10%',borderWidth :0.5, marginTop:'-10%' 
+        
+      },
+      bot : {
+         flex:1 ,
+         alignItems: 'center',
+         
+      },
+      anh:{
+         marginTop:'-10%'
+      }
+
+
 });
 export default styles;
